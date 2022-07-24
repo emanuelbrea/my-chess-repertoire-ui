144b23d1d9fc7b4e08c359ed8145c51fbd9e8963
emanuelbrea <ebrea@uade.edu.ar>
2022-07-24 18:44:19 -0300

    Updated svg url

--- src/Line.js
+++ src/Line.js
@@ -25,6 +25,7 @@ export default function Line({fen, color, addVariant, currentDepth, removeMoves}
         }), requestOptions)
             .then(res => res.json())
             .catch((error) => {
+                console.log(error)
                 return {'success' : false}
             })
 
