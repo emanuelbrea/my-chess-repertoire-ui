31f52c3f2ce970203d865002af46074b37b55871
emanuelbrea <ebrea@uade.edu.ar>
2022-07-24 22:57:15 -0300

    Added log

--- src/Line.js
+++ src/Line.js
@@ -19,6 +19,9 @@ export default function Line({fen, color, addVariant, currentDepth, removeMoves}
         const requestOptions = {
             method: 'GET',
         };
+        console.log(process)
+        console.log(process.env)
+        console.log(process.env.NEXT_PUBLIC_HOST)
         const moves = await fetch(process.env.NEXT_PUBLIC_HOST+'/repertoire/?' + new URLSearchParams({
             fen: fen,
             color: color
