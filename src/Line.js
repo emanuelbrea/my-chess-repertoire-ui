57b10d8db826d59cd92ac5bb41f76cc8b802603f
emanuelbrea <ebrea@uade.edu.ar>
2022-07-26 00:04:21 -0300

    Added api subdomain

--- src/Line.js
+++ src/Line.js
@@ -19,7 +19,7 @@ export default function Line({fen, color, addVariant, currentDepth, removeMoves}
         const requestOptions = {
             method: 'GET',
         };
-        const moves = await fetch(process.env.NEXT_PUBLIC_HOST+'/repertoire/?' + new URLSearchParams({
+        const moves = await fetch(process.env.NEXT_PUBLIC_HOST+'/api/repertoire/?' + new URLSearchParams({
             fen: fen,
             color: color
         }), requestOptions)
@@ -37,7 +37,7 @@ export default function Line({fen, color, addVariant, currentDepth, removeMoves}
         const requestOptions = {
             method: 'PUT',
         };
-        const moves = await fetch(process.env.NEXT_PUBLIC_HOST+'/repertoire/?' + new URLSearchParams({
+        const moves = await fetch(process.env.NEXT_PUBLIC_HOST+'/api/repertoire/?' + new URLSearchParams({
             fen: fen,
             color: color,
             move: move
@@ -52,7 +52,7 @@ export default function Line({fen, color, addVariant, currentDepth, removeMoves}
         const requestOptions = {
             method: 'PATCH',
         };
-        const moves = await fetch(process.env.NEXT_PUBLIC_HOST+'/repertoire/?' + new URLSearchParams({
+        const moves = await fetch(process.env.NEXT_PUBLIC_HOST+'/api/repertoire/?' + new URLSearchParams({
             fen: fen,
             color: color
         }), requestOptions)
--- src/util.js
+++ src/util.js
@@ -1,3 +1,3 @@
 export function getSvgUrl(fen, move, color) {
-    return process.env.NEXT_PUBLIC_HOST + '/position/svg?fen=' + encodeURIComponent(fen) + '&move=' + encodeURIComponent(move) + '&color=' + color;
+    return process.env.NEXT_PUBLIC_HOST + '/api/position/svg?fen=' + encodeURIComponent(fen) + '&move=' + encodeURIComponent(move) + '&color=' + color;
 }
